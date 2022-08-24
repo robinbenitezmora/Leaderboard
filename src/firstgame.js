1d1cdba621132d8347eed2b60fe809cc19feb2bb
robinbenitezmora <robinbenitez1990@gmail.com>
2022-08-23 21:14:53 -0500

    Add code lines in 'firstgame.js' file

---
+++ src/firstgame.js
@@ -0,0 +1,17 @@
+const game = async () => {
+  let gameId = [];
+  const gameIdResult = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: 'First Game',
+    }),
+  });
+  const gameIdJson = await gameIdResult.json();
+  gameId = (gameIdJson.showscores).split(' ');
+  return gameId;
+};
+
+module.exports = game;
